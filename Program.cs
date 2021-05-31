26b9c5a767b5d755ff6451f07f412a1bd624c295
giuliohome <giuliohome@gmail.com>
2021-05-31 13:52:43 +0200

    run wpf in memory

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Navigation;
 
 namespace crypt
 {
@@ -30,6 +33,7 @@ namespace crypt
                 }
             }
         }
+        [STAThread]
         static void Main(string[] args)
         {
             if (args.Contains("xml"))
@@ -130,7 +134,52 @@ namespace crypt
                 Console.WriteLine("verified: " + (verified ? "Yes" : "No"));
                 return;
             }
-            Console.WriteLine("valid verbs are only: xml | sign | verify");
+
+            if (args.Contains("run-in-memory"))
+            {
+                int input_idx = Array.IndexOf(args, "-input");
+                string input_path;
+                if (input_idx > -1 && input_idx < args.Length - 1 && !args[input_idx + 1].StartsWith("-"))
+                {
+                    input_path = args[input_idx + 1];
+                }
+                else
+                {
+                    Console.WriteLine("-input missing");
+                    return;
+                }
+                int publickey_idx = Array.IndexOf(args, "-publickey");
+                string publickey_path;
+                if (publickey_idx > -1 && publickey_idx < args.Length - 1 && !args[publickey_idx + 1].StartsWith("-"))
+                {
+                    publickey_path = args[publickey_idx + 1];
+                }
+                else
+                {
+                    Console.WriteLine("-publickey missing");
+                    return;
+                }
+                int signature_idx = Array.IndexOf(args, "-signature");
+                string signature_path;
+                if (signature_idx > -1 && signature_idx < args.Length - 1 && !args[signature_idx + 1].StartsWith("-"))
+                {
+                    signature_path = args[signature_idx + 1];
+                }
+                else
+                {
+                    Console.WriteLine("-signature missing");
+                    return;
+                }
+                if (args.Length > 7)
+                {
+                    Console.WriteLine("too many args for verb run-in-memory");
+                    return;
+                }
+                Console.WriteLine("run-in-memory pubkey={0} input={1} signature={2} of {3} args", publickey_path, input_path, signature_path, args.Length);
+                RunSigned(input_path, publickey_path, signature_path);
+                return;
+            }
+            Console.WriteLine("valid verbs are only: xml | sign | verify | run-in-memory");
         }
         private static void Sign(string secretkey_path, string input_path, string output_path)
         {
@@ -159,6 +208,37 @@ namespace crypt
                 byte[] signed = File.ReadAllBytes(signature_path);
                 bool verified = crypt.VerifyData(bytes, signed, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
                 return verified;
+            }
+        }
+        private static void RunSigned(string input_path, string publickey_path, string signature_path)
+        {
+            bool verified = VerifySigned(input_path, publickey_path, signature_path);
+            if (!verified)
+            {
+                Console.WriteLine("Not verified! Can't launch!");
+                return;
+            }
+            Console.WriteLine("running " + input_path + " in memory");
+            byte[] bytes = File.ReadAllBytes(input_path);
+            Assembly assembly = Assembly.Load(bytes);
+
+            var app = typeof(Application);
+
+            var field = app.GetField("_resourceAssembly", BindingFlags.NonPublic | BindingFlags.Static);
+            field.SetValue(null, assembly);
+
+            var helper = typeof(BaseUriHelper);
+            var property = helper.GetProperty("ResourceAssembly", BindingFlags.NonPublic | BindingFlags.Static);
+            property.SetValue(null, assembly, null);
+
+
+            try
+            {
+                assembly.EntryPoint.Invoke(null, new object[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
             }
         }
     }
